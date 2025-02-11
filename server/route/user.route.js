4e2961f0c5277fae2b3472f4ec8f0d1b4ffb0b72
Astik Gupta <guptastik123@gmail.com>
2025-02-11 14:42:04 +0530

    first commit

---
+++ server/route/user.route.js
@@ -0,0 +1,34 @@
+import express from 'express';
+import {
+  loginController,
+  logoutController,
+  registerUserController,
+  uploadAvatar,
+  updateUserDetails,
+  forgotPasswordController,
+  verifyEmailController,
+  userDetails,verifyForgotPasswordOtp,
+  resetpassword,
+  refreshToken,
+} from "../controllers/user.controller.js";
+import auth from '../middleware/auth.js'
+import upload from '../middleware/multer.js';
+
+
+const userRouter = express.Router();
+
+userRouter.post('/register', registerUserController);
+userRouter.post('/verify-email', verifyEmailController);
+userRouter.post('/login',loginController)
+userRouter.get('/logout',auth,logoutController)
+userRouter.put('/upload-avatar',auth,upload.single('avatar'),uploadAvatar)
+userRouter.put('/update-user',auth,updateUserDetails)
+userRouter.put('/forgot-password',forgotPasswordController)
+userRouter.put('/verify-forgot-password-otp',verifyForgotPasswordOtp)
+userRouter.put('/reset-password',resetpassword)
+userRouter.post('/refresh-token',refreshToken)
+userRouter.get("/user-details", auth, userDetails);
+
+export default userRouter;
+
+
